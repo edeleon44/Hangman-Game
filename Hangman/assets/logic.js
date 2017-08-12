f7476dd2ddfa76f68295f2fd14756fa58747516b
edeleon44 <edeleon44@gmail.com>
2017-08-11 21:40:01 -0500

    Add files via upload

---
+++ Hangman/assets/logic.js
@@ -0,0 +1,121 @@
+//GLOBAL VARIABLES --> REACH TO ALL OF THE FILE
+// -----------------------------------------------------------------
+//Arrays and variable for holding data
+var wordOptions = ["poker", "chip", "felt", "fish"];
+var selectedWord = " ";
+var lettersInWord = [];
+var numBlanks = 0;
+var blanksAndSuccesses = []; // P O _ _ _ (example..)
+var wrongLetters = [];
+
+//Game Counters
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 9;
+
+// FUNCTIONS --> REUSABLE BLOCKS OF THAT I WILL CALL UPON WHEN NEEDED
+//--------------------------------------------------------------------
+
+function startGame() {
+	selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+	lettersInWord = selectedWord.split("");
+	numBlanks = lettersInWord.length;
+
+
+		//reset
+	guessesLeft = 9;
+	wrongLetters = [];
+	blanksAndSuccesses = [];
+
+		
+
+	//Populate blanks and successes with right number of blanks
+	for (var i=0; i<numBlanks; i++){
+		blanksAndSuccesses.push("_");
+	}
+
+
+		//_ _ _ _ h3 tag needs to have the ability to change according to word selected
+	//The blankAndSuccesses.JOIN removes the comma's assosiated with the word chosen.
+	document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+	document.getElementById("numGuesses").innerHTML = guessesLeft;
+	document.getElementById("winCounter").innerHTML - winCount;
+	document.getElementById("lossCounter").innerHTML = lossCount;
+}	
+	//TEST
+	console.log(selectedWord);
+		
+	
+
+
+	function checkLetters(letter){
+		//First checks if the letter is anywhere in the word --> Then every spot it exists
+		 
+		 var isLetterInWord = false;
+
+		 for (var i=0; i<numBlanks; i++) {
+		 	if(selectedWord[i] == letter) {
+		 	isLetterInWord = true;
+		 }
+	}
+
+	//Check where letter exists
+	if(isLetterInWord) {
+	for (var i=0; i<numBlanks; i++) {
+		if(selectedWord[i] == letter) {
+
+			blanksAndSuccesses[i] = letter;
+			}
+		}
+	}
+
+	//Letter was not found
+	else {
+		wrongLetters.push(letter);
+		guessesLeft --
+	}
+
+	function roundComplete(){
+		console.log("winCount: " + winCount + " | Loss Count: " + lossCount + " | Guesses Left" + numGuesses);
+
+		//Each Round we need an update 
+		document.getElementById("guessesLeft").innerHTML=guessesLeft;
+		document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+		document.getElementById("wrongGuesses").innerHTML = wrongGuesses.join(" ");
+		//Check if won
+		if (lettersInWord.toString() == blanksAndSuccesses.toString()) {
+			winCount++;
+			alert("You Won!");
+
+			//Update Win projection in HTML
+			document.getElementById("winCounter").innerHTML = winCount;
+
+			startGame();
+		}
+
+		//Check if user lost
+		else if (guessesLeft ==0) {
+			lossCount++;
+			alert("You Lose!");
+		//Update Loss projection in HTML
+		document.getElementById("lossCounter").innerHTML = lossCount;
+		startGame();
+		}
+	}
+
+//MAIN PROCESS --> WHERE THE STUFF IS GOING DOWN "TRAPHOUSE"
+//--------------------------------------------------------------------
+
+//Kicks the Game off..
+
+startGame();
+
+//Register Key Clicks
+document.onkeyup = function(event){
+	var letterGuessed = string.fromCharCode(event.keyCode).toLowerCase();
+	checkLetters(letterGuessed); }
+	roundComplete();
+
+}
+//Testing Clicks
+
